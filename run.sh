7bbe26eeff18c48e5ed09d519a2c5332b3bc8777
Minimum <s5550055@yahoo.com.tw>
2015-10-15 15:24:18 +0800

    Check status before tailing log.

--- run.sh
+++ run.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+
+WAIT_SERVICE_READY=10
+
+function check_service(){
+  status=$($WORKDIR/control status)
+  echo $status | grep -q "stoped"
+  if [ $? -eq 0 ] ; then
+    return 1
+  else
+    return 0
+  fi
+}
+
 tar -zxf $PACKDIR/$PACKFILE -C $WORKDIR
 cp $CONFIGDIR/$CONFIGFILE $WORKDIR
 $WORKDIR/control restart
-$WORKDIR/control tail
+sleep $WAIT_SERVICE_READY
+check_service
+if [ $? -eq 0 ] ; then
+  $WORKDIR/control tail
+else
+  echo "Failed to start."
+  exit 1
+fi
